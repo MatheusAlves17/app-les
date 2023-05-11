9a16f46683d8f9f72fd0e4e2238dddb9542a0172
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-05-11 19:21:41 -0300

    feat: sign-up-form and sign-up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignupFormComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/signup-form/signup-form.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent {
+  @Input() btnText!: string;
+}
